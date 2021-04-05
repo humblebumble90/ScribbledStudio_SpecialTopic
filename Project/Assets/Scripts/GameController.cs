58368b57a2af5b63b2ab937568c9ea82c2440fd7
HyungseokLeee <hyngsuki@gmail.com>
2021-04-05 01:14:06 -0400

    Player pointer is added

--- Project/Assets/Scripts/GameController.cs
+++ Project/Assets/Scripts/GameController.cs
@@ -9,11 +9,21 @@ public class GameController : MonoBehaviour
     private int pScore;
     public Text bScoreTxt;
     public Text pScoreTxt;
+    public GameObject point;
+    private float rot;
     // Start is called before the first frame update
     void Start()
     {
         bScore = 0;
         pScore = 0;
+        rot = 0f;
+        //point.transform.localScale = new Vector3(10, 1, 1);
+
+    }
+    private void FixedUpdate()
+    {
+        rot += 0.01f;
+        point.transform.rotation *= Quaternion.Euler(0, rot, 0);
     }
 
     public void Score(AgentSoccer.Team color)
